d522521b42a7ba5f66be309f03c68cc5d7f3604c
Magnus Bjerke Vik <mbvett@gmail.com>
2015-12-10 22:42:30 +0100

    Fix highpass filters not working because of alpha

--- ipap/core/image.py
+++ ipap/core/image.py
@@ -21,14 +21,20 @@ class Image:
         rdata = np.reshape(np.asarray(pilimage.getdata(band=0)), pilimage.size[::-1])
         gdata = np.reshape(np.asarray(pilimage.getdata(band=1)), pilimage.size[::-1])
         bdata = np.reshape(np.asarray(pilimage.getdata(band=2)), pilimage.size[::-1])
-        adata = np.reshape(np.asarray(pilimage.getdata(band=3)), pilimage.size[::-1])
+
+        image._rgb = np.array([
+            rdata,
+            gdata,
+            bdata
+        ])
 
         image._dft = np.array([
             np.fft.fftshift(np.fft.fft2(rdata)),
             np.fft.fftshift(np.fft.fft2(gdata)),
-            np.fft.fftshift(np.fft.fft2(bdata)),
-            np.fft.fftshift(np.fft.fft2(adata))
+            np.fft.fftshift(np.fft.fft2(bdata))
         ])
+
+        image._update_data()
 
         return image
 
@@ -38,24 +44,43 @@ class Image:
     def getmode(self):
         return self._pil.mode
 
+    def _update_data(self):
+        self.data = np.empty((self._rgb[0].shape[0], self._rgb[0].shape[1], 4), dtype=np.uint8)
+        for row in range(0, self.data.shape[0]):
+            for col in range(0, self.data.shape[1]):
+                for chan in range(0, 3):
+                    self.data[row][col][chan] = self._rgb[chan][row][col]
+                self.data[row][col][3] = 255
+
+    def _add_alpha(self, rgb):
+        oldshape = self.dft[0].shape
+        return np.concatenate(
+            (
+                rgb,
+                np.ones((oldshape[0], oldshape[1], 1)) * 255
+            ),
+            axis=2
+        )
+
+    @property
+    def rgb(self):
+        return self._rgb
+
     @property
     def dft(self):
         return self._dft
 
     @dft.setter
-    def dft(self, data):
-        self._dft = data
-        size = self._dft[0].shape[1]
-        data = np.concatenate(
-            (
-                np.split(np.fft.ifft2(np.fft.ifftshift(self._dft[0])).astype(np.uint8), size, axis=1),
-                np.split(np.fft.ifft2(np.fft.ifftshift(self._dft[1])).astype(np.uint8), size, axis=1),
-                np.split(np.fft.ifft2(np.fft.ifftshift(self._dft[2])).astype(np.uint8), size, axis=1),
-                np.split(np.fft.ifft2(np.fft.ifftshift(self._dft[3])).astype(np.uint8), size, axis=1)
-            ),
-            axis=2
-        )
-        self.data = np.transpose(data, axes=[1, 0, 2])
+    def dft(self, dft_data):
+        self._dft = dft_data
+
+        self._rgb = np.array([
+            np.fft.ifft2(np.fft.ifftshift(self._dft[0])).astype(np.uint8),
+            np.fft.ifft2(np.fft.ifftshift(self._dft[1])).astype(np.uint8),
+            np.fft.ifft2(np.fft.ifftshift(self._dft[2])).astype(np.uint8)
+        ])
+
+        self._update_data()
 
     @property
     def dft_rgb(self):
@@ -70,16 +95,6 @@ class Image:
         rgb = np.transpose(rgb, axes=[1, 0, 2])
 
         return rgb
-
-    def _add_alpha(self, rgb):
-        oldshape = self.dft[0].shape
-        return np.concatenate(
-            (
-                rgb,
-                np.ones((oldshape[0], oldshape[1], 1)) * 255
-            ),
-            axis=2
-        )
 
     @property
     def dft_magnitude(self):
